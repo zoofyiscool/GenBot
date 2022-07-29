94cd1e1465dfaa7492b57552f868d82958ca0b28
zoofyiscool <ouxiez@tutanota.com>
2022-07-29 23:21:44 +0100

    command prefix:   index.cjs

--- index.cjs
+++ index.cjs
@@ -1,14 +1,20 @@
 const { Client } = require("revolt.js");
 require('dotenv').config();
-
 let client = new Client();
+const prefix = "doas ";
 
 client.on("ready", async () =>
     console.info(`Logged in as ${client.user.username}!`),
 );
 
 client.on("message", async (message) => {
-    if (message.content === "ping") {
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    
+    if (! message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
+    if (command === "ping") {
         message.channel.sendMessage("pong");
     }
 });
